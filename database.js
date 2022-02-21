1595b584ee823257ef5fbf90f6bcdd97a61eb929
Urvashi-Satra <84577974+Urvashi-Satra@users.noreply.github.com>
2022-02-21 12:51:24 +0530

    Update database.js

--- database.js
+++ database.js
@@ -8,6 +8,3 @@ var conn = mysql.createConnection({
 
 module.exports = conn;
 
-//wait app.js run pe it should go on index file jo nahi hai and it will come in routes 
-//this data on console is of db barabar wohi i was thinking 
-//and index.js file hai vo coinmarket site ka data vala code hai
